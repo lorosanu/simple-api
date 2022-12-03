3c3e45ec29d1b484d8b917056539abfdc16e6be1
Luiza Sarzyniec <sarzyniec@textkernel.nl>
2022-12-03 14:19:04 +0100

    Add scripts to send random requests to the service

---
+++ send_mixed_requests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+target_endpoint="http://localhost:8080/doc_length"
+text="This is a random text. "
+
+for i in `seq 1 1000000`
+do
+    # define a random longer length text (can go over the 1000 chars limit)
+    N=$(( $RANDOM % 100 + 1 ))
+    doctext=$(printf "%${N}s" | sed 's/ /'"$text"'/g')
+    shuffled_doctext=$(echo "$doctext" | fold -w1 | shuf | tr -d '\n')
+
+    curl -s -X POST "$target_endpoint" -H "Content-Type: application/json" -d '{"text": "'"$shuffled_doctext"'"}' > /dev/null; done
+done
