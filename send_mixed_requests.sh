0cb596618f8bd90d3413d9f4c29afd256c01de4a
Luiza Sarzyniec <sarzyniec@textkernel.nl>
2022-12-04 17:03:34 +0100

    Adapt the script to the new limit

--- send_mixed_requests.sh
+++ send_mixed_requests.sh
@@ -3,14 +3,17 @@
 set -euo pipefail
 
 target_endpoint="http://localhost:8080/doc_length"
-text="This is a random text. "
+text="${1:-This is a random text. }"
+
+max_limit=1000
+factor=$(( $max_limit / ${#text} * 2 ))
 
 for i in `seq 1 1000000`
 do
-    # define a random longer length text (can go over the 1000 chars limit)
-    N=$(( $RANDOM % 100 + 1 ))
+    # define a random longer length text (can go over the limit)
+    N=$(( $RANDOM % $factor + 1 ))
     doctext=$(printf "%${N}s" | sed 's/ /'"$text"'/g')
     shuffled_doctext=$(echo "$doctext" | fold -w1 | shuf | tr -d '\n')
 
-    curl -s -X POST "$target_endpoint" -H "Content-Type: application/json" -d '{"text": "'"$shuffled_doctext"'"}' > /dev/null; done
+    curl -s -X POST "$target_endpoint" -H "Content-Type: application/json" -d '{"text": "'"$shuffled_doctext"'"}' > /dev/null
 done
