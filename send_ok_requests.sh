8185e621bdb8298e9d8ca0ea7ef1c610d9984fa0
Luiza Sarzyniec <sarzyniec@textkernel.nl>
2022-12-04 11:08:04 +0100

    Update scripts to allow user input and generate longer texts

--- send_ok_requests.sh
+++ send_ok_requests.sh
@@ -3,14 +3,17 @@
 set -euo pipefail
 
 target_endpoint="http://localhost:8080/doc_length"
-text="This is a random text. "
+text="${1:-This is a random text. }"
+
+max_limit=50000
+factor=$(( $max_limit / ${#text} - 1 ))
 
 for i in `seq 1 1000000`
 do
-    # define a random longer length text (always under 1000 chars limit)
-    N=$(( $RANDOM % 40 + 1 ))
+    # define a random longer length text (always under the limit)
+    N=$(( $RANDOM % $factor + 1 ))
     doctext=$(printf "%${N}s" | sed 's/ /'"$text"'/g')
     shuffled_doctext=$(echo "$doctext" | fold -w1 | shuf | tr -d '\n')
 
-    curl -s -X POST "$target_endpoint" -H "Content-Type: application/json" -d '{"text": "'"$shuffled_doctext"'"}' > /dev/null; done
+    curl -s -X POST "$target_endpoint" -H "Content-Type: application/json" -d '{"text": "'"$shuffled_doctext"'"}' > /dev/null
 done
